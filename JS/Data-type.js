7eacfebf4e8b9784e4f44dbddbf1c38778858d05
Kurbonoffdev <bfammily5@gmail.com>
2023-11-25 18:53:21 +0500

    ozgaruvchilar

---
+++ JS/Data-type.js
@@ -0,0 +1,49 @@
+// Number
+
+let number = 5.6; 
+console.log(-8/0); // Infinity matematikada 8 nolga bo'linsa Infinity chiqadi
+console.log('Bekzod'* 8); // NaN
+
+// Strings 
+
+const ClientName = 'Bekzod';
+
+
+// Boolean
+
+const isMarried = false ;
+const isCircleEarth = true;
+
+// True  xa | Tog'ri 
+// False yoq | Notog'ri
+
+// null 
+
+console.log(Clientage);
+
+// null yoq o'zgaruvchini chaqirsak NUll chiqadi
+
+
+// Undefined
+
+let age;
+
+console.log(age);
+
+// undefined o'zgaruvchi bor lkn malumot yoq
+
+
+
+// Object
+const thief = {
+//Key: Valeu
+  age:30,//number
+  jacket:'black',//String
+  isLong: false,//Boolean
+}
+
+
+// array massiv bu object malumot turini bir qismi xisoblanadi
+
+const colots = ['red','blue',[],{}]; //Massiv
+
