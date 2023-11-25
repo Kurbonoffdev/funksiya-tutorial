42545a1748c3535782e17e18aa5196ffe4c36955
Kurbonoffdev <bfammily5@gmail.com>
2023-11-25 18:54:36 +0500

    foydalanuvchi bilan muloqot

---
+++ JS/muloqot.js
@@ -0,0 +1,22 @@
+// foydalanuvchi bilan oddiy muloqot 
+
+alert('hello JavaScript');
+
+
+// confirm bu foydalanuvchiga true false qiymatdagi savollar berish uchun
+confirm('san shu yerdamisan ?');
+
+
+// promt bu foydalanuvchiga savol berish va javob olish string formatda
+prompt('san qayerdasan');
+
+
+// amaliyot 
+
+const userName = [];
+
+// arrayga malumot qo'shish
+userName[0] = prompt('ismingiz nima?');
+
+// Natija
+console.log(userName);
