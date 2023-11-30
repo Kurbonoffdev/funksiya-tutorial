0261727c4be80791a8c28c5f1026338f7ca18cef
Kurbonoffdev <bfammily5@gmail.com>
2023-11-30 16:41:28 +0500

    css tutorial

---
+++ main.js
@@ -0,0 +1,59 @@
+// muloqot
+
+// alert('siz rostdan ham ushbu saytdan chiqmoqchimisz');
+
+// +prompt('nechi yoshdasz');
+
+// let boolean = confirm('siz 18 yoshdamisz')
+
+// console.log(boolean);
+
+
+//  array 
+
+// let userData =[ 'bekzod','jaxongir','deniz','fayzullo'];
+
+// userData[3] = 'jasur';
+
+// console.log(userData);
+
+// let a = prompt('ismingizni kiritng');
+// let b = prompt('ismingizni kiritng');
+// let c = prompt('ismingizni kiritng');
+// let d = prompt('ismingizni kiritng');
+// let v = prompt('ismingizni kiritng');
+
+// let dataType = [a,b,c,d,v];
+
+
+
+// console.log(dataType);
+
+
+// let a = +prompt('son kiriting');
+
+// let b = +prompt('son kiriting');
+
+// let x = a+b;
+
+// let Db = [x];
+
+
+// console.log(Db);
+
+
+// JavaScripdan turib Dinamic style berish
+
+// const text = document.querySelector('h1');
+// const box = document.querySelector('.age');
+
+// text.style.color = 'red';
+
+// text.style.textAlign = 'center';
+
+// box.style.width = '400px';
+// box.style.height = '400px';
+// box.style.backgroundColor = 'blue';
+// box.style.borderRadius = '50%';
+// box.style.marginLeft = '600px';
+
